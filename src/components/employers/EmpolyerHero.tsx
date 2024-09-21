a9f4b20da972ec214fc03c84a5a6feb00ab16383
Sufyan <Sufyan265@users.noreply.github.com>
2024-09-21 18:56:04 +0500

    hide hero content in md

--- src/components/employers/EmpolyerHero.tsx
+++ src/components/employers/EmpolyerHero.tsx
@@ -3,14 +3,14 @@
 const EmpolyerHero = () => {
   return (
     <div className="relative w-full h-full bg-cover bg-center transition-all" style={{ backgroundImage: 'url("https://www.texicare.com/wp-content/uploads/2024/02/Warehouse-Birds-Eye-View.jpg")' }}>
-      <div className="container mx-auto lg:px-28 md:px-20">
-        <div className="text-center text-white px-4 lg:px-10 py-20 md:py-40">
+      <div className="container mx-auto lg:px-28 md:px-20 md:py-0 py-32">
+        <div className="text-center text-white px-4 lg:px-10 py-20 md:py-40 md:block hidden">
           <h1 className="text-4xl md:text-[75px] md:leading-[75px] font-[600]">
             Your business is only as healthy as the people in it.
           </h1>
           <button
             className="bg-primary hover:bg-white border-2 hover:text-primary border-primary px-6 py-3 my-3 font-bold rounded-full text-white text-xl font-avenir-bold"
-            style={{fontSize: '20px !important', lineHeight: '20px !important'}}
+            style={{ fontSize: '20px !important', lineHeight: '20px !important' }}
           >
             Find an Agent
           </button>
